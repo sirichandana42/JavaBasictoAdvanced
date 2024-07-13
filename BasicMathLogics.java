03ee3ea2af2258256e76b7d4f34da27dda56ea1e
Siri Chandana Guntur <110028208+sirichandana42@users.noreply.github.com>
2024-07-13 18:27:53 +0530

    Update BasicMathLogics.java

--- BasicMathLogics.java
+++ BasicMathLogics.java
@@ -289,3 +289,26 @@ public class basiccodes
 
     }
 }
+
+//no.of trialing zeros in a factorial
+//solution hint: Keep dividing n by powers of 5 and update count
+import java.util.Scanner;
+public class basiccodes{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        findTrailingZeros(n);
+    }
+
+    public static void findTrailingZeros(int n) {
+        int count = 0;
+        for (int i = 5; n / i >= 1; i *= 5) {
+            count += n / i;
+        }
+        
+        System.out.print(count);
+    }
+}
+
+
