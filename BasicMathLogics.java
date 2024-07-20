fa8b775f3777a99a89535032d6a11bfdad7f2849
Siri Chandana Guntur <110028208+sirichandana42@users.noreply.github.com>
2024-07-20 19:47:02 +0530

    Update BasicMathLogics.java

--- BasicMathLogics.java
+++ BasicMathLogics.java
@@ -452,3 +452,38 @@ public class basiccodes {
     }
 }
 
+//Implement the Sieve of Eratosthenes to find all prime numbers up to N.
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class basiccodes 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);  
+        isPrime[0] = isPrime[1] = false;  
+
+        for (int i = 2; i * i <= n; i++) 
+        {
+            if (isPrime[i]) 
+            {
+                for (int j = i * i; j <= n; j += i) 
+                {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        for (int i = 2; i <= n; i++) 
+        {
+            if (isPrime[i]) 
+            {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
